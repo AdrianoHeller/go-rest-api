90f758079073fea14b140200f7f59a19e722578c
adrianohellermylla <adrianohellermylla@gmail.com>
2022-07-15 22:09:12 -0300

    feat: Add helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,10 +1,60 @@
 package helpers
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net/http"
+	"strings"
 	"time"
 )
+
+func HandleCustomErrors(w http.ResponseWriter, err error) error {
+	var sErr *json.SyntaxError
+	var unmarshallError *json.UnmarshalTypeError
+
+	switch {
+
+	case errors.As(err, &sErr):
+		msg := fmt.Sprintf("request body contains badly-formed JSON at position %d", sErr.Offset)
+		http.Error(w, msg, http.StatusBadRequest)
+		return nil
+
+	case errors.Is(err, io.ErrUnexpectedEOF):
+		msg := "request body contains badly-formed JSON"
+		http.Error(w, msg, http.StatusBadRequest)
+		return nil
+
+	case err.Error() == "http: request body too large ":
+		msg := "request body must not be larger than 1MB"
+		http.Error(w, msg, http.StatusRequestEntityTooLarge)
+		return nil
+
+	case errors.As(err, &unmarshallError):
+		msg := fmt.Sprintf("request body contains invalid value for %q field (at position %d)", unmarshallError.Field, unmarshallError.Offset)
+		http.Error(w, msg, http.StatusBadRequest)
+		return nil
+
+	case strings.HasPrefix(err.Error(), "json: unknown field "):
+		fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+		msg := fmt.Sprintf("request body contains unknown field %s", fieldName)
+		http.Error(w, msg, http.StatusBadRequest)
+		return nil
+
+	case errors.Is(err, io.EOF):
+		msg := "request body cannot be empty"
+		http.Error(w, msg, http.StatusBadRequest)
+		return nil
+
+	default:
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return nil
+	}
+}
 
 func CreateRandomToken(tokenLength int) (string, error) {
 	rand.Seed(time.Now().UnixNano())
@@ -20,5 +70,32 @@ func CreateRandomToken(tokenLength int) (string, error) {
 		return string(token), nil
 	} else {
 		return "", errors.New("token length cannot be lower than 10")
+	}
+}
+
+func ConvertToJson(targetData interface{}) ([]byte, error) {
+	convertedData, err := json.Marshal(targetData)
+	if err != nil {
+		msg := fmt.Sprintf("error found: %s", err)
+		return nil, errors.New(msg)
+	}
+	return convertedData, nil
+}
+
+func ConvertFromJson(marshaledData []byte) (interface{}, error) {
+	var placeholder interface{}
+	err := json.Unmarshal(marshaledData, &placeholder)
+	if err != nil {
+		msg := fmt.Sprintf("error found: %s", err)
+		return nil, errors.New(msg)
+	}
+	return placeholder, nil
+}
+
+func CheckValidHttpMethod(r *http.Request, acceptableMethod string) (bool, error) {
+	if r.Method == acceptableMethod {
+		return true, nil
+	} else {
+		return false, errors.New("wrong method provided")
 	}
 }
---
+++ models/wallet.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"api/helpers"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TransStatus int
+
+const (
+	Processed = iota
+	OnHold
+	Cancelled
+)
+
+type Transaction struct {
+	Id     uuid.UUID   `json:"id"`
+	From   string      `json:"from"`
+	To     string      `json:"to"`
+	Amount int         `json:"amount"`
+	Status TransStatus `json:"st"`
+}
+
+type Wallet struct {
+	Id           string        `json:"id"`
+	Owner        string        `json:"owner"`
+	CreatedAt    time.Time     `json:"created_at"`
+	Transactions []Transaction `json:"transactions"`
+}
+
+func (h *Wallet) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1040000)
+
+	var wlt Wallet
+
+	dec := json.NewDecoder(r.Body)
+
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(&wlt)
+
+	if err != nil {
+		helpers.HandleCustomErrors(w, err)
+		return
+	}
+
+	data, err := helpers.ConvertToJson(r.Body)
+
+	w.WriteHeader(http.StatusOK)
+	_, e := w.Write(data)
+
+	if e != nil {
+		msg := fmt.Sprintf("Error found: %s", e)
+		http.Error(w, msg, http.StatusInternalServerError)
+	}
+}
